3ddbde71cbee798986e05de3f14da1053259b4b7
kkzhang <kkzhang1029@gmail.com>
2022-03-20 13:09:28 +0800

    finish two-sum

--- src/main/java/com/kkzhang/easy/TwoSum.java
+++ src/main/java/com/kkzhang/easy/TwoSum.java
@@ -1,12 +1,24 @@
 package com.kkzhang.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/two-sum/
- * 
  */
 public class TwoSum {
     public static int[] solution(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int tar = target - nums[i];
+            if (map.containsKey(tar)) {
+                res[0] = map.get(tar);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
+        }
         return res;
     }
 }
---
+++ src/main/java/com/kkzhang/medium/ThreeSum.java
@@ -0,0 +1,12 @@
+package com.kkzhang.medium;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/3sum/
+ */
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+    }
+}
