2003954098732d37884bfa5dd805035a32b2752d
kkzhang <kkzhang1029@gmail.com>
2022-08-28 16:44:37 +0800

    binary search: sqrt

---
+++ src/main/java/com/kkzhang/medium/LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,14 @@
+package com.kkzhang.medium;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
+ */
+public class LongestWordInDictionaryThroughDeleting {
+    // not solved
+    public String findLongestWord(String s, List<String> dictionary) {
+        return "";
+    }
+}
+
---
+++ src/test/java/com/kkzhang/easy/SqrtTest.java
@@ -0,0 +1,13 @@
+package com.kkzhang.easy;
+
+import org.junit.Test;
+
+public class SqrtTest {
+    private Sqrt solution = new Sqrt();
+
+    @Test
+    public void testSqrt(){
+        int x = 3;
+        System.out.print(solution.mySqrt(x));
+    }
+}
