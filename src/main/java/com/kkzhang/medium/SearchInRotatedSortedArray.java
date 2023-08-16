6262f0778de7f05101a035f1006575edd856e814
kkzhang <kkzhang1029@gmail.com>
2023-08-16 23:12:02 +0800

    [pin] 33. Search in Rotated Sorted Array

--- src/main/java/com/kkzhang/medium/SearchInRotatedSortedArray.java
+++ src/main/java/com/kkzhang/medium/SearchInRotatedSortedArray.java
@@ -5,6 +5,34 @@ package com.kkzhang.medium;
  */
 public class SearchInRotatedSortedArray {
 
+    public int search3(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] <= nums[right]) {
+                if (target > nums[mid] && target < nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] >= nums[left]) {
+                if (target > nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     /**
      * 解题思路：
      * 这道题可以使用二分查找来解决。
@@ -35,13 +63,17 @@ public class SearchInRotatedSortedArray {
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] >= nums[left]) {
+            if (nums[mid] == nums[left]) {
+                left = mid + 1;
+            } else if (nums[mid] == nums[right]) {
+                right = mid - 1;
+            } else if (nums[mid] > nums[left]) {
                 if (target < nums[mid] && target >= nums[left]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
+            } else if (nums[mid] < nums[right]) {
                 if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
