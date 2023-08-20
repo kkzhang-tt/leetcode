51cda1c0faacaaae30ca4e335961c288ea9ebe2c
kkzhang <kkzhang1029@gmail.com>
2023-08-20 12:27:59 +0800

    234. Palindrome Linked List

--- src/main/java/com/kkzhang/easy/PalindromeLinkedList.java
+++ src/main/java/com/kkzhang/easy/PalindromeLinkedList.java
@@ -6,9 +6,82 @@ import com.kkzhang.common.ListNode;
  * https://leetcode.com/problems/palindrome-linked-list/
  */
 public class PalindromeLinkedList {
+
+    // leetcode 答案
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome3(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode pre = null;
+        ListNode cur = slow.next;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        slow.next = null;
+
+        ListNode p1 = head;
+        ListNode p2 = pre;
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        if (p1 == null) {
+            return p2.next == null ? true : false;
+        } else {
+            return p1.next == null ? true : false;
+        }
+    }
+
     // 先找到中间节点，之后反转后半部分，再比较前后部分是否是回文
     // 思路对，但是没有通过全部用例
-    public boolean isPalindrome(ListNode head) {
+    public boolean isPalindrome2(ListNode head) {
         if (head == null) {
             return false;
         }
