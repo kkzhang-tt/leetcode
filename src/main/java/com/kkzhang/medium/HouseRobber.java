cc6927ce080c6e304c7d5fb786bbe184c6fc3216
kkzhang <kkzhang1029@gmail.com>
2023-09-16 16:40:45 +0800

    [pin] 198. House Robber

--- src/main/java/com/kkzhang/medium/HouseRobber.java
+++ src/main/java/com/kkzhang/medium/HouseRobber.java
@@ -23,16 +23,16 @@ public class HouseRobber {
     public int rob2(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        
+
         dp[0] = nums[0];
         if (n > 1) {
             dp[1] = Math.max(nums[0], nums[1]);
         }
-        
+
         for (int i = 2; i < n; i++) {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        
+
         return dp[n - 1];
     }
 
@@ -56,5 +56,22 @@ public class HouseRobber {
             max = Math.max(max, f[i]);
         }
         return max;
+    }
+
+    public int rob3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
     }
 }
