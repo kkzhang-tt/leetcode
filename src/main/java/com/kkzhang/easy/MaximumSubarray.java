fe31d4b8d1b6e15010323c1071d1c8df3c00285d
kkzhang <kkzhang1029@gmail.com>
2023-09-25 23:38:20 +0800

    9. Palindrome Number

--- src/main/java/com/kkzhang/easy/MaximumSubarray.java
+++ src/main/java/com/kkzhang/easy/MaximumSubarray.java
@@ -21,4 +21,23 @@ public class MaximumSubarray {
         }
         return max;
     }
+
+    public int maxSubArray2(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
 }
