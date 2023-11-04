093341f92c1f53105eb7c21fb32ef84d4cbb0aef
kkzhang <kkzhang1029@gmail.com>
2023-11-04 12:50:35 +0800

    [pin] 17. Letter Combinations of a Phone Number

---
+++ src/main/java/com/kkzhang/medium/PermutationsII.java
@@ -0,0 +1,15 @@
+package com.kkzhang.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/permutations-ii/description/
+ */
+public class PermutationsII {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        return null;
+    }
+    
+}
