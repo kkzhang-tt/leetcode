8b1f692d9a3fceaad9116b40f130212a3daa954b
kkzhang <kkzhang1029@gmail.com>
2024-01-10 00:12:29 +0800

    206. Reverse Linked List

--- src/main/java/com/kkzhang/easy/ReverseLinkedList.java
+++ src/main/java/com/kkzhang/easy/ReverseLinkedList.java
@@ -20,4 +20,20 @@ public class ReverseLinkedList {
         }
         return pre;
     }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
 }
